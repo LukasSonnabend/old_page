b52898a2972b3d642a1fe5d5d2647a77b44bc611
LukasSonnabend <lukassonnabend@outlook.com>
2020-07-17 18:39:01 +0200

    updated Dom game and rengefunc.js

--- rangefunc.js
+++ rangefunc.js
@@ -42,22 +42,71 @@ kluesNumbs2 = kluesNumbs.map(x => x+1);
 console.log(kluesNumbs2);
 
 //Turn an play Logic
-function getDOM(y, x){
+
+
+function getDOM(y, x)
+  {
+  console.log("Turn status: " + turnStatus);
+  if (boardState[y][x] == "O" || boardState[y][x] == "X"){
+  if (turnStatus < 1){
+    turnStatus = 1;
   content = document.getElementById(y+","+x).innerHTML;
   if (content == "X" || content == "O"){
     document.getElementById(y+","+x).innerHTML = " ";
     turn += 1;
     console.log(turn);
   }
+
+  console.log(y + "," + x);
+  console.log( boardState[y][x] );
+    //get possible moves
+    if (boardState[y][x] == "O"){
+      oldX = x;
+      oldY = y;
+      boardElement = boardState[y][x];
+      oldDiv = y +","+x;
+      oldChar = boardState[y][x]
+      moves = y-1 + "," + (x-1);
+      if(x > 0){
+        oldColorM1 =  document.getElementById(moves).style.background;
+        document.getElementById(moves).style.background = "lightgrey";
+      }
+      
+      moves = y-1 + "," + (x+1);
+      if(x < 7){
+        oldColorM2 =  document.getElementById(moves).style.background;
+        document.getElementById(moves).style.background = "lightgrey";  
+      } 
+    }
+  }
+  else{
+    if( y == oldY && x == oldX){
+      console.log("setting:" + oldDiv + " " + oldChar);
+      document.getElementById(oldDiv).innerHTML = oldChar;
+      // document.getElementById(moves).style.background = oldColorM1;
+      moves = y-1 + "," + (x-1);
+      if(x > 0){
+      document.getElementById(moves).style.background = oldColorM1;
+      }
+      moves = y-1 + "," + (x+1);
+      if(x < 7){
+      document.getElementById(moves).style.background = oldColorM2; 
+      }
+      turnStatus = 0; 
+    }
+
+  }
+}
+
   
-  console.log(y+","+x);
+
   //turn indicator handling
-    if (turn % 2 == 0){
-      document.getElementById("turnIndicator").style.borderTop = "2px solid green";
-      document.getElementById("turnIndicator").style.borderBottom = "2px solid white";  
-    }
-    else{
-      document.getElementById("turnIndicator").style.borderTop = "2px solid white";
-      document.getElementById("turnIndicator").style.borderBottom = "2px solid green";  
-    }
+    // if (turn % 2 == 0){
+    //   document.getElementById("turnIndicator").style.borderTop = "2px solid green";
+    //   document.getElementById("turnIndicator").style.borderBottom = "2px solid white";  
+    // }
+    // else{
+    //   document.getElementById("turnIndicator").style.borderTop = "2px solid white";
+    //   document.getElementById("turnIndicator").style.borderBottom = "2px solid green";  
+    // }
 }
